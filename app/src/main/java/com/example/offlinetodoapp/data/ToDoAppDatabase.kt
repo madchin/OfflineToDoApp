fa219d3cc77bdc3242cda95214e91fddbde98135
MarcinLoj <m.lojewski1998@gmail.com>
2023-07-09 21:29:16 +0200

    remove app container and its equivalents

--- app/src/main/java/com/example/offlinetodoapp/data/ToDoAppDatabase.kt
+++ app/src/main/java/com/example/offlinetodoapp/data/ToDoAppDatabase.kt
@@ -1,25 +1,9 @@
 package com.example.offlinetodoapp.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Task::class], version = 1, exportSchema = false)
+@Database(entities = [Task::class], version = 2, exportSchema = false)
 abstract class ToDoAppDatabase : RoomDatabase() {
-    abstract fun taskDao(): TaskDao
-
-    companion object {
-        @Volatile
-        private var Instance: ToDoAppDatabase? = null
-
-        fun getDatabase(context: Context): ToDoAppDatabase {
-            return Instance ?: synchronized(this) {
-                Room.databaseBuilder(context, ToDoAppDatabase::class.java, "item_database")
-                    .fallbackToDestructiveMigration()
-                    .build()
-                    .also { Instance = it }
-            }
-        }
-    }
+    abstract val taskDao: TaskDao
 }
